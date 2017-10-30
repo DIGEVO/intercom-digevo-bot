c39d96b9164144f6a0b1e9f7ef1d25358e084475
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-10-29 19:38:42 -0500

    testing2

--- libs/utils/index.js
+++ libs/utils/index.js
@@ -30,7 +30,7 @@ const self = module.exports = {
             name: self.getName(session.message),
             address: session.message.address
         });
-        console.log(`${userId} - ${JSON.stringify(session.message.address)}`);
+        console.log(`${userId} - ${JSON.stringify(flow.cache.get(userId))}`);
     },
 
     saveIncomingMessageIntoIntercom: (session, next) => {
